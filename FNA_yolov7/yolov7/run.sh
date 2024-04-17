dc612d9a83093bf99cbe2d439c1b6bc1ac9abff9
Kanghyun Kim <powergkrry@gmail.com>
2024-04-17 16:33:39 -0400

    Init for FNA_yolov7

--- FNA_yolov7/yolov7/run.sh
+++ FNA_yolov7/yolov7/run.sh
@@ -10,7 +10,7 @@ WEIGHTS="${ROOT}/yolov7/runs/train/${NAME}/weights/best.pt"
 IMAGE_SIZE="1536"
 
 if [ "$1" == "train" ]; then
-    python3 train_aux.py --device 0 --name $NAME --img $IMAGE_SIZE --data $DATA --weights 'yolov7-e6e_training.pt' --epochs 2 --batch-size 8 --workers 8 --cfg cfg/training/yolov7-e6e.yaml --hyp $HYP --single-cls
+    python3 train_aux.py --device 0 --name $NAME --img $IMAGE_SIZE --data $DATA --weights 'yolov7-e6e_training.pt' --epochs 200 --batch-size 8 --workers 8 --cfg cfg/training/yolov7-e6e.yaml --hyp $HYP --single-cls
 
 elif [ "$1" == "test" ]; then
     python3 test.py --data $DATA --img $IMAGE_SIZE --batch 1 --device 0 --weights $WEIGHTS --name "${NAME}_t" --single-cls --task 'test'
